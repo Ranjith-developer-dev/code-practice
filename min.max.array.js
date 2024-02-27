70c900d4238ef5993101ca26d663f24a6021ad6b
Ranjith-developer-dev <ranjithraffiqjob@gmail.com>
2024-02-27 08:13:52 +0530

    min and max of array

---
+++ min.max.array.js
@@ -0,0 +1,25 @@
+
+function max(arr) {
+    let max = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) max = arr[i]
+    }
+    console.log(max)
+}
+max([2, 5, 3, 1, 8, 4])
+
+const array = [2, 5, 3, 1, 8, 4]
+
+const min = array.reduce((cur, acc) => {
+
+    return cur < acc ? cur : acc;
+});
+console.log("🚀 ~ min ~ min:", min)
+
+
+const max1 = array.reduce((curval, nextval) => {
+
+    return curval > nextval ? curval : nextval;
+});
+
+console.log("🚀 ~ max1 ~ max1:", max1)
